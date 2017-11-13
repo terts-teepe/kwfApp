391bc1b1500d6850acab8054dad81d21ae155783
Rawan Abou Dehn <rawan.ad7@gmail.com>
2017-11-13 11:46:37 +0100

    Make a few changes
    Change Middelware and the libraries

--- app.js
+++ app.js
@@ -1,26 +1,23 @@
 // Libraries
-const express = require('express');
-const path = require('path');
-const session = require('express-session');
-const bodyParser = require('body-parser');
-const db = require(__dirname + '/models/database.js')
-const app = express();
+const express = require('express'),
+	    path = require('path'),
+      session = require('express-session'),
+      bodyParser = require('body-parser'),
+      db = require(__dirname + '/models/database.js'),
+      app = express(),
 
 // Including usage of routes
-const login = require('./routes/login');
-const logout = require('./routes/logout');
+      login = require('./routes/login'),
+      logout = require('./routes/logout'),
 
-const bcrypt = require('bcrypt');
+      bcrypt = require('bcrypt');
 
 // View engine setup
-app.set('views', path.join(__dirname, '/views'));
+app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'pug');
 
 // Middleware
-app.use(bodyParser.json()); //activates middleware of body-parser -- request.body is nu beschikbaar onder elke app.post
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use('/', bodyParser()); //creates key-value pairs request.body in app.post, e.g. request.body.username
 
 app.use(express.static(path.join(__dirname, 'public'))); //To serve static files such as images, CSS files, and JavaScript files, use the express.static built-in middleware function in Express.
 
