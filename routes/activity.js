3cc893b0565d89a75addda2f452d01b3a6febff6
Rawan Abou Dehn <rawan.ad7@gmail.com>
2017-11-21 13:59:20 +0100

    Fix bug in activity page

--- routes/activity.js
+++ routes/activity.js
@@ -36,15 +36,29 @@ router.post('/', (req,res)=>{
 	let friends = req.body.friends;
 	let time = req.body.time;
 	let location = req.body.location;
-	for (var i = 0; i < friends.length; i++) {
+	console.log('friends')
+	console.log(friends)
+	if(Array.isArray(friends)){
+		for (var i = 0; i < friends.length; i++) {
+			db.Activity.create({
+				plannerId: currentUserId,
+				categorie: categorie,
+				time: time,
+				friend: friends[i],
+				location: location
+			})
+		}
+	}
+	else {
 		db.Activity.create({
 			plannerId: currentUserId,
 			categorie: categorie,
 			time: time,
-			friend: friends[i],
+			friend: friends,
 			location: location
 		})
 	}
+
 	res.redirect('/index')
 })
 
