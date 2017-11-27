235ce2dd16aa224d6dd4acd3bc6e74efcb4aacc0
Felix Teepe <terts@live.nl>
2017-11-27 14:04:01 +0100

    send sms to multiple numbers

--- routes/messages.js
+++ routes/messages.js
@@ -12,20 +12,33 @@ router.get('/', function(req, res) {
   res.render('messages');
 });
 
-router.post('/', (req, res) => {
 
+router.post('/', (req, res) => {
+	var people = [{phonenumber: '+31620528245', name: 'Terts'},
+				  {phonenumber: '+31614845655', name: 'Rawan'}];
 	var recipient = req.body.to;
 	var name = req.body.name;
+	console.log(people[0].phonenumber);
+	console.log(people[1].phonenumber);
 
-	client.messages.create({
-	    body: `Hello ${name}, do you want to join the vriendendienst network!`,
-	    to: `${recipient}`,  // Text this number
-	    from: '+3197004498785' // From a valid Twilio number
-	})
-	.then((message) => {
-		console.log(message.sid)
-		res.send('Message sent')
-	});
+	for(var i=0; i<people.length; i++) {
+
+		client.messages.create({
+		    body: `Hello ${people[i].name}, do you want to join the vriendendienst network! Check it out here: https://share.proto.io/FAFPRN/`,
+		    to: people[i].phonenumber,  // Text this number
+		    from: '+3197004498785' // From a valid Twilio number
+		})
+		.then((message) => {
+			console.log(people.length);
+			if(i === people.length - 1) {
+				console.log(people.length);
+				console.log(message.sid)
+				res.redirect('/inviteFriends');
+				// res.send(`Your invitation has been sent to your friend ${people[i].name}! Send another invitation here: `)
+			}
+		});
+	}
+
 });
 
 
