6b3fa6145c7f9adbc24947975c2a355c6e3299e4
Felix Teepe <terts@live.nl>
2017-11-28 11:15:30 +0100

    added add more button to messages file

---
+++ src/public/js/messages.js
@@ -0,0 +1,4 @@
+function add_fields() {
+	console.log('test')
+    document.getElementById('wrapper').innerHTML += '<div class="form-group"><input type="text" name="name" placeholder="Friends Name" class="form-control"/></div><div class="form-group"><input type="text" name="to" placeholder="Friends Number" class="form-control"/></div>';
+	}
