c283ff568db9f13fb458efe43b28ea2ca692e47d
Rawan Abou Dehn <rawan.ad7@gmail.com>
2017-12-07 12:17:03 +0100

    Try to implement ChatBot

--- routes/chatbot.js
+++ routes/chatbot.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/database.js');
 const bodyParser = require('body-parser');
-var ConversationV1 = require('watson-developer-cloud/conversation/v1');
+const Conversation = require('watson-developer-cloud/conversation/v1');
+
+'use strict';
+
     // Set up Conversation service wrapper.
-    var conversation = new ConversationV1({
+    var conversation = new Conversation({
       username: 'd9770ce8-59fa-4134-88fd-e42fdfc86642', // replace with username from service key
       password: 'JLSm1G40w02H', // replace with password from service key
       path: { workspace_id: 'd11b2b05-b64c-4914-9ccf-3db1480c8b05' }, // replace with workspace ID
@@ -31,10 +34,68 @@ router.get('/', function(req, res) {
         console.log(response.output.text[0]);
       }
     }
+    res.render('chatbot')
 })
 
+
+
+
+// Endpoint to be call from the client side
+router.post('/', function(req, res) {
+  var workspace = process.env.WORKSPACE_ID || 'd11b2b05-b64c-4914-9ccf-3db1480c8b05';
+  if (!workspace || workspace === 'd11b2b05-b64c-4914-9ccf-3db1480c8b05') {
+    return res.json({
+      'output': {
+        'text': 'The app has not been configured with a <b>WORKSPACE_ID</b> environment variable. Please refer to the ' + '<a href="https://github.com/watson-developer-cloud/conversation-simple">README</a> documentation on how to set this variable. <br>' + 'Once a workspace has been defined the intents may be imported from ' + '<a href="https://github.com/watson-developer-cloud/conversation-simple/blob/master/training/car_workspace.json">here</a> in order to get a working application.'
+      }
+    });
+  }
+  var payload = {
+    workspace_id: workspace,
+    context: req.body.context || {},
+    input: req.body.input || {}
+  };
+
+  // Send the input to the conversation service
+  conversation.message(payload, function(err, data) {
+    if (err) {
+      return res.status(err.code || 500).json(err);
+    }
+    return res.json(updateMessage(payload, data));
+  });
+});
+
+/**
+ * Updates the response text using the intent confidence
+ * @param  {Object} input The request to the Conversation service
+ * @param  {Object} response The response from the Conversation service
+ * @return {Object}          The response with the updated message
+ */
+function updateMessage(input, response) {
+  var responseText = null;
+  if (!response.output) {
+    response.output = {};
+  } else {
+    return response;
+  }
+  if (response.intents && response.intents[0]) {
+    var intent = response.intents[0];
+    // Depending on the confidence of the response the app can return different messages.
+    // The confidence will vary depending on how well the system is trained. The service will always try to assign
+    // a class/intent to the input. If the confidence is low, then it suggests the service is unsure of the
+    // user's intent . In these cases it is usually best to return a disambiguation message
+    // ('I did not understand your intent, please rephrase your question', etc..)
+    if (intent.confidence >= 0.75) {
+      responseText = 'I understood your intent was ' + intent.intent;
+    } else if (intent.confidence >= 0.5) {
+      responseText = 'I think your intent was ' + intent.intent;
+    } else {
+      responseText = 'I did not understand your intent';
+    }
+  }
+  response.output.text = responseText;
+  return response;
+}
+
+
 module.exports = router;
-
-
-
-
