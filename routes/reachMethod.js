bae1b8c4740b2d8468783c578ba0276e5b24456e
Felix Teepe <terts@live.nl>
2017-12-11 16:58:22 +0100

    added styling

--- routes/reachMethod.js
+++ routes/reachMethod.js
@@ -12,8 +12,72 @@ router.get('/', (req, res) => {
 router.post('/', (req,res) => {
 	let name = req.query.name;
 	let email = req.body.email;
-	let number = req.body.number;
-	res.redirect(`/password?name=${name}&email=${email}&number=${number}`)
+	let phoneNumber = req.body.phoneNumber;
+	let password = req.body.password;
+	
+
+	db.User.create({
+	            name: name,
+	            password: password,
+	            email: email,
+	            phoneNumber: phoneNumber
+	        })
+	        .then((user) => {
+	            req.session.user = user;
+	            res.redirect('/addFriends?message=' + encodeURIComponent("User created"));
+	            // Alert that user registered worked
+        	})
 })
 
 module.exports = router;
+
+
+
+
+
+//
+
+// const express = require('express');
+// const router = express.Router();
+// const db = require('../models/database.js');
+// const bodyParser = require('body-parser');
+
+
+// router.get('/', (req, res) => {
+// 	let name = req.query.name;
+// 	let email = req.query.email;
+// 	let number = req.query.number;
+// 	res.render('password', {name: name, email: email, number: number});
+// });
+
+// router.post('/', (req,res)=> {
+// 	let name = req.query.name;
+// 	let email = req.query.email;
+// 	let phoneNumber = req.query.number;
+// 	let password = req.body.password;
+//   	// Check if Username already exists
+//   	db.User.findOne({
+//       	where: {
+//         	email: email
+//       	}
+//     })
+//     .then((user) => {
+//       	if (user) {
+//         	res.redirect('/register?message=' + encodeURIComponent("Username already exists"));
+//         	// Put alert that username already exist
+//       	} else {
+//         	db.User.create({
+// 	            name: name,
+// 	            password: password,
+// 	            email: email,
+// 	            phoneNumber: phoneNumber
+// 	        })
+// 	        .then((user) => {
+// 	            req.session.user = user;
+// 	            res.redirect('/addFriends?message=' + encodeURIComponent("User created"));
+// 	            // Alert that user registered worked
+//         	})
+//  /*     	}
+//     })*/
+// })
+// module.exports = router;
