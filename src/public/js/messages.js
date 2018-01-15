7d93718e77074addd8acd43c9eac2bc4cea9d331
Felix Teepe <terts@live.nl>
2018-01-15 14:57:51 +0100

    styled sms and gmail pages

--- src/public/js/messages.js
+++ src/public/js/messages.js
@@ -5,5 +5,5 @@
 }*/
 
 $('#more_fields').click(function(){
-	$(this).before('<div class="form-group"><input type="text" name="name" placeholder="Naam" class="form-control"/></div><div class="form-group"><input type="text" name="to" value="+316" class="form-control"/></div>')
+	$(this).before('<div class="NaamStyle"><input type="text" name="name" placeholder="Naam" class="form-control"/></div><div class="form-group"><input type="text" name="to" placeholder="Telefoonnummer (+316)" class="form-control"/></div>')
 })
