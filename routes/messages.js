68265cd40749bd589d73348fc919711f51863569
Felix Teepe <terts@live.nl>
2018-01-16 14:29:37 +0100

    added styling

--- routes/messages.js
+++ routes/messages.js
@@ -43,7 +43,7 @@ router.post('/', (req, res) => {
 				client.messages.create({
 				    // body: `Hello ${people[i].name}, would you like to join the vriendendienst network! Check it out here: https://share.proto.io/FAFPRN/`,
 
-				    body: `Hallo ${people[i].name} Je vriend ${user.name} wil je graag uitnodigen voor Vriendendienst, ga naar de website om deel uit te maken van het netwerk: https://share.proto.io/FAFPRN/.`,
+				    body: `Hallo ${people[i].name} Je vriend ${currentUserName} wil je graag uitnodigen voor Vriendendienst, ga naar de website om deel uit te maken van het netwerk: https://share.proto.io/FAFPRN/.`,
 				    to: people[i].phonenumber,  // Text this number
 				    from: '+3197004498785' // From a valid Twilio number
 				})
@@ -67,7 +67,7 @@ router.post('/', (req, res) => {
 		client.messages.create({
 /*		    body: `Hello ${people[0].name}, Would you like to join the vriendendienst network! Check it out here: https://share.proto.io/V9VKHX/`,*/
 
-			body: `Hallo ${people[0].name} Je vriend ${user.name} wil je graag uitnodigen voor Vriendendienst, ga naar de website om deel uit te maken van het netwerk: https://share.proto.io/FAFPRN/.`,
+			body: `Hallo ${people[0].name} Je vriend ${currentUserName} wil je graag uitnodigen voor Vriendendienst, ga naar de website om deel uit te maken van het netwerk: https://share.proto.io/FAFPRN/.`,
 		    to: people[0].phonenumber,  // Text this number
 		    from: '+3197004498785' // From a valid Twilio number
 		})
