a591a1f977f3f3576b9223c83208058163ea80fd
Felix Teepe <terts@live.nl>
2018-01-17 14:53:15 +0100

    deployment works

--- app.js
+++ app.js
@@ -145,7 +145,7 @@ app.get('/auth/provider/callback',
                                       failureRedirect: '/' })); */
 
 // Running server
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 8080, () => {
   console.log('Server running')
 })
 
